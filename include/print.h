d69f68940c84e90f8f4ddf8718ae9ee2d5745cee
Filipe Franciel Utzig <futzig@parks.com.br>
2015-03-19 14:19:55 -0300

    Added lib print

---
+++ include/print.h
@@ -0,0 +1,19 @@
+// print.h
+//
+// Copyright (c) 2015 Filipe Utzig. All rights reserved.
+//
+// Initial version by Filipe Utzig <filipeutzig@gmail.com> on 3/19/15.
+//
+// The key words "MUST", "MUST NOT", "REQUIRED", "SHALL", "SHALL NOT",
+// "SHOULD", "SHOULD NOT", "RECOMMENDED",  "MAY", and "OPTIONAL" in
+// this document are to be interpreted as described in RFC 2119.
+//
+// Some useful print functions
+//
+
+#ifndef __FUTZIG_PRINT_H
+#define __FUTZIG_PRINT_H
+
+void print_time (struct timeval init, struct timeval end);
+
+#endif // __FUTZIG_PRINT_H
