721a0731ff54e4b330cb81def859ea29c3950bde
Filipe Utzig <filipeutzig@gmail.com>
2015-03-22 18:01:03 -0300

    Fixing headers

--- sequential/main.c
+++ sequential/main.c
@@ -1,3 +1,16 @@
+// main.c
+//
+// Copyright (c) 2015 Filipe Utzig. All rights reserved.
+//
+// Initial version by Filipe Utzig <filipeutzig@gmail.com> on 3/19/15.
+//
+// The key words "MUST", "MUST NOT", "REQUIRED", "SHALL", "SHALL NOT",
+// "SHOULD", "SHOULD NOT", "RECOMMENDED",  "MAY", and "OPTIONAL" in
+// this document are to be interpreted as described in RFC 2119.
+//
+// Yet another sort app using rank_sort algothim (Sequential implementation)
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
