499b3b20fed5015df0655e0d07b35a72c8b3f767
Filipe Utzig <filipeutzig@gmail.com>
2015-03-22 18:00:41 -0300

    Fixing header

--- libs/print.c
+++ libs/print.c
@@ -1,3 +1,16 @@
+// print.c
+//
+// Copyright (c) 2015 Filipe Utzig. All rights reserved.
+//
+// Initial version by Filipe Utzig <filipeutzig@gmail.com> on 3/19/15.
+//
+// The key words "MUST", "MUST NOT", "REQUIRED", "SHALL", "SHALL NOT",
+// "SHOULD", "SHOULD NOT", "RECOMMENDED",  "MAY", and "OPTIONAL" in
+// this document are to be interpreted as described in RFC 2119.
+//
+// Some useful print functions
+//
+
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
